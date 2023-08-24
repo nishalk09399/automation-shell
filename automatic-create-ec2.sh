335ce060c413da3955340cdda290809fad61bee6
nishal <nishalk9399@gmail.com>
2023-08-24 16:33:11 +0530

    adding automatic ec2 script

--- automatic-create-ec2.sh
+++ automatic-create-ec2.sh
@@ -18,10 +18,33 @@ do
        INSTANCE_TYPE="t3.micro"
     fi
     echo "Creating $i...........Instance"
+
+    #To create the automatic ec2 instances in aws.
+
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE  --security-group-ids $SECURITY_GROUP 
     --tag-specifications "ResourceType=Instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
 
     echo "created $i instaces: $IP_ADDRESS"
+
+    #to update and create the DNS Records in aws. 
+
+    aws route53 change-resource-record-sets --hosted-zone-id Z07869251AQBK6AKJKM03 --change-batch
+    {
+
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                                    "Name": "$i.$DOMAIN_NAME",
+                                    "Type": "A",
+                                    "TTL": 300,
+                                    "ResourceRecords": [{ "Value": "$IP_ADDRESS"}]
+                                                                                      
+                                                        
+                        }}]
+    }              
+
+
+
 
 done
 
