391e45e7433a9abe7d8974a6de5a5b0f4d5647a9
nishal <nishalk9399@gmail.com>
2023-08-24 16:00:10 +0530

    adding automatic ec2 script

--- automatic-create-ec2.sh
+++ automatic-create-ec2.sh
@@ -3,7 +3,10 @@
 NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "cart" "user" "shipping" "payments" "dispatch" "web")
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
-SECURITY_GROUP=sg-034b8c610c8a714d3 
+SECURITY_GROUP=sg-034b8c610c8a714d3
+R="\e[31m" 
+G="\e[32m" 
+Y="\e[33m"
 
 #conditions
 #if mysql or mongo instance should be in t3.medium and all other items instance should be t3.micro
@@ -11,19 +14,19 @@ SECURITY_GROUP=sg-034b8c610c8a714d3
 for i in "${NAMES[@]}"
 do
 
-if [ [ $i == "mongodb" || $i == "mysql"] ]
+if [[ $i == "mongodb" || $i == "mysql"]]
 then
     INSTANCE_TYPE="t3.medium"
 else
     INSTANCE_TYPE="t3.micro"
     
 fi
-    echo "Creating $i...........Instance"
+    echo "$Y Creating $i...........Instance"
 
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE  --security-group-ids $SECURITY_GROUP 
     --tag-specifications "ResourceType=Instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
 
-    echo "created $i instaces: $IP_ADDRESS"
+    echo "$G created $i instaces: $IP_ADDRESS"
 
 done
 
